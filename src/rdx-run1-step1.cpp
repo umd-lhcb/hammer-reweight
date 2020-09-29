4f9cb2eb7a505bfa69b48bad143bf1f15e1446c9
Yipeng Sun <syp@umd.edu>
2020-09-30 00:55:26 +0800

    Working on rdx run 1 FF reweighting.

---
+++ src/rdx-run1-step1.cpp
@@ -0,0 +1,11 @@
+// Author: Yipeng Sun
+// Last Change: Wed Sep 30, 2020 at 12:55 AM +0800
+// Description: FF reweighting for R(D(*)) run 1, step 1 ntuples.
+// Based on:
+// https://github.com/ZishuoYang/my-hammer-reweighting/blob/master/Bc2JpsiMuNu.cc
+
+#include <Hammer/Hammer.hh>
+#include <Hammer/Math/FourMomentum.hh>
+#include <Hammer/Particle.hh>
+#include <Hammer/Process.hh>
+#include <Hammer/Tools/HammerRoot.hh>
