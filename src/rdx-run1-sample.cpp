ccfc8811659386585969e5183bbb821c69603e14
Yipeng Sun <syp@umd.edu>
2020-10-08 22:02:05 +0800

    Create a output tree to store hammer weights. Not working yet.

--- src/rdx-run1-sample.cpp
+++ src/rdx-run1-sample.cpp
@@ -1,5 +1,5 @@
 // Author: Yipeng Sun
-// Last Change: Thu Oct 08, 2020 at 01:59 AM +0800
+// Last Change: Thu Oct 08, 2020 at 10:01 PM +0800
 // Description: FF reweighting for R(D(*)) run 1, step 1 ntuples.
 // Based on:
 //   https://github.com/ZishuoYang/my-hammer-reweighting/blob/master/Bc2JpsiMuNu.cc
@@ -98,6 +98,82 @@ void reweight_dst(TFile* input_file, TFile* output_file,
   TTreeReaderValue<Double_t> anu_mu_true_pz(reader, "anu_mu_true_pz");
 
   // Define output branches ////////////////////////////////////////////////////
+  ULong64_t eventNumber_out;
+  output.Branch("eventNumber", &eventNumber_out);
+  UInt_t runNumber_out;
+  output.Branch("runNumber", &runNumber_out);
+  Int_t ham_proc_id_out;
+  output.Branch("ham_proc_id", &ham_proc_id_out);
+
+  // Setup HAMMER //////////////////////////////////////////////////////////////
+  Hammer::Hammer   ham{};
+  Hammer::IOBuffer ham_buf;
+
+  ham.includeDecay("BD*TauNu");
+  ham.addFFScheme("Scheme1", {{"BD*", "BGL"}});
+  ham.setOptions("BctoJpsiBGL: {dvec: [0., 0., 0.] }");
+  ham.setFFInputScheme({{"BD*", "CLN"}});
+
+  ham.setUnits("GeV");  // This is the default, but let's be explicit.
+
+  ham.initRun();
+
+  while (reader.Next()) {
+    eventNumber_out = *eventNumber;
+    runNumber_out   = *runNumber;
+
+    auto B   = particle(*b_true_pe, *b_true_px, *b_true_py, *b_true_pz, *b_id);
+    auto Dst = particle(*dst_true_pe, *dst_true_px, *dst_true_py, *dst_true_pz,
+                        *dst_id);
+    auto SlowPi = particle(*spi_true_pe, *spi_true_px, *spi_true_py,
+                           *spi_true_pz, *spi_id);
+    auto D0 =
+        particle(*d0_true_pe, *d0_true_px, *d0_true_py, *d0_true_pz, *d0_id);
+    auto K = particle(*k_true_pe, *k_true_px, *k_true_py, *k_true_pz, *k_id);
+    auto Pi =
+        particle(*pi_true_pe, *pi_true_px, *pi_true_py, *pi_true_pz, *pi_id);
+    auto Mu =
+        particle(*mu_true_pe, *mu_true_px, *mu_true_py, *mu_true_pz, *mu_id);
+    auto Anti_Nu_Mu = particle(*anu_mu_true_pe, *anu_mu_true_px,
+                               *anu_mu_true_py, *anu_mu_true_pz, *anu_mu_id);
+    auto Anti_Nu_Tau =
+        particle(*anu_tau_true_pe, *anu_tau_true_px, *anu_tau_true_py,
+                 *anu_tau_true_pz, *anu_tau_id);
+    auto Nu_Tau = particle(*nu_tau_true_pe, *nu_tau_true_px, *nu_tau_true_py,
+                           *nu_tau_true_pz, *nu_tau_id);
+
+    Hammer::Process proc;
+
+    auto B_idx           = proc.addParticle(B);
+    auto Dst_idx         = proc.addParticle(Dst);
+    auto SlowPi_idx      = proc.addParticle(SlowPi);
+    auto D0_idx          = proc.addParticle(D0);
+    auto K_idx           = proc.addParticle(K);
+    auto Pi_idx          = proc.addParticle(Pi);
+    auto Mu_idx          = proc.addParticle(Mu);
+    auto Anti_Nu_Mu_idx  = proc.addParticle(Anti_Nu_Mu);
+    auto Anti_Nu_Tau_idx = proc.addParticle(Anti_Nu_Tau);
+    auto Nu_Tau_idx      = proc.addParticle(Nu_Tau);
+
+    proc.addVertex(
+        B_idx, {Dst_idx, Mu_idx, Anti_Nu_Mu_idx, Anti_Nu_Tau_idx, Nu_Tau_idx});
+    proc.addVertex(Dst_idx, {D0_idx, SlowPi_idx});
+    proc.addVertex(D0_idx, {K_idx, Pi_idx});
+
+    ham.initEvent();
+    auto proc_id = ham.addProcess(proc);
+
+    if (proc_id != 0) {
+      ham_proc_id_out = proc_id;
+
+      ham.processEvent();
+      ham_buf = ham.saveEventWeights();
+
+      output.Fill();
+    }
+  }
+
+  output_file->Write("", TObject::kOverwrite);
 }
 
 int main(int, char** argv) {
