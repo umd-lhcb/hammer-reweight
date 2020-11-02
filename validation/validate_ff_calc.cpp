f94e861f0f4f7f8167f9edbba93410ff7d107597
Yipeng Sun <syp@umd.edu>
2020-11-02 12:44:53 +0100

    Update legend.

--- validation/validate_ff_calc.cpp
+++ validation/validate_ff_calc.cpp
@@ -1,14 +1,17 @@
 // Author: Yipeng Sun
 // License: GPLv2
 // Description: Validation of FF reweighting from ISGW2 -> CLN
-// Last Change: Mon Nov 02, 2020 at 12:11 PM +0100
+// Last Change: Mon Nov 02, 2020 at 12:44 PM +0100
 
+#include <iostream>
 #include <string>
 
 #include <TCanvas.h>
 #include <TFile.h>
 #include <TH1D.h>
+#include <TLegend.h>
 #include <TROOT.h>
+#include <TStyle.h>
 
 #include <ff_dstaunu.hpp>
 
@@ -33,8 +36,10 @@ TH1D q2_histo(BMeson b_type, FFType ff_type, Double_t m_lep, Int_t nbinsx,
   ff_calc.SetMasses(b_type);
 
   for (auto bin = 1; bin <= nbinsx; bin++) {
-    auto q2 = histo.GetXaxis()->GetBinCenter(bin);
-    histo.SetBinContent(bin, ff_calc.Compute(q2, ff_type, m_lep * m_lep));
+    auto q2      = histo.GetXaxis()->GetBinCenter(bin);
+    auto q2_dist = ff_calc.Compute(q2, ff_type, m_lep);
+
+    histo.SetBinContent(bin, q2_dist);
   }
 
   return histo;
@@ -45,19 +50,38 @@ int main(int, char** argv) {
   string output_dir = argv[2];
 
   gROOT->SetBatch(kTRUE);
-
+  gStyle->SetOptStat(0);
   auto canvas = new TCanvas("canvas", "FF validation", 4000, 3000);
+https:  // root.cern.ch/doc/master/classTLegend.html
+  // Reference CLN
+  auto histo_ref_cln_B0ToDstTauNu = q2_histo(
+      BMeson::Neutral, FFType::CLN, m_Tau, 400, 3, 12, "CLN", "Reference CLN");
 
-  // Reference CLN vs reweighted
-  auto histo_ref_cln_B0ToDstTauNu =
-      q2_histo(BMeson::Neutral, FFType::CLN, m_Tau, 400, 3, 12, "histo_ref_cln",
-               "Reference CLN");
-
+  histo_ref_cln_B0ToDstTauNu.SetLineWidth(2);
+  histo_ref_cln_B0ToDstTauNu.SetLineColor(kRed);
   histo_ref_cln_B0ToDstTauNu.Draw();
+
+  // Reference ISGW2
+  auto histo_ref_isgw2_B0ToDstTauNu =
+      q2_histo(BMeson::Neutral, FFType::ISGW2, m_Tau, 400, 3, 12, "ISGW2",
+               "Reference ISGW2");
+  histo_ref_isgw2_B0ToDstTauNu.SetLineWidth(2);
+  histo_ref_isgw2_B0ToDstTauNu.SetLineColor(kBlue);
+  histo_ref_isgw2_B0ToDstTauNu.Draw("same");
+
+  // Legend
+  auto legend = new TLegend(0.1, 0.8, 0.3, 0.9);
+  legend->SetTextSize(0.03);
+  legend->AddEntry(&histo_ref_cln_B0ToDstTauNu, "CLN", "f");
+  legend->AddEntry(&histo_ref_isgw2_B0ToDstTauNu, "ISGW2", "f");
+  legend->Draw();
+  // canvas->BuildLegend();
+
   canvas->Update();
 
   canvas->Print((output_dir + "/ff_cln.png").c_str());
 
   delete input_file;
   delete canvas;
+  delete legend;
 }
