3f235cdc886376d134bbe8a8d6af8ad8440b610d
Yipeng Sun <syp@umd.edu>
2020-11-08 02:33:19 +0100

    Add some debugging helpers; still working on rescaling.

--- validation/validate_ff_calc.cpp
+++ validation/validate_ff_calc.cpp
@@ -1,7 +1,7 @@
 // Author: Yipeng Sun
 // License: GPLv2
 // Description: Validation of FF reweighting from ISGW2 -> CLN
-// Last Change: Sun Nov 08, 2020 at 02:02 AM +0100
+// Last Change: Sun Nov 08, 2020 at 02:33 AM +0100
 
 #include <iostream>
 #include <string>
@@ -28,9 +28,8 @@ const Double_t m_Bplus = 5.2792;
 const Double_t m_Tau   = 1.7768;
 
 // q2 distributions with a particular FF parameterization
-TH1D q2_histo(BMeson b_type, FFType ff_type, Double_t m_lep,
-              Long64_t normalization, const char* name, const char* title,
-              Int_t nbinsx, Double_t xlow, Double_t xup,
+TH1D q2_histo(BMeson b_type, FFType ff_type, Double_t m_lep, const char* name,
+              const char* title, Int_t nbinsx, Double_t xlow, Double_t xup,
               Option_t* scale_opt = "width") {
   auto histo   = TH1D(name, title, nbinsx, xlow, xup);
   auto ff_calc = BToDstaunu{};
@@ -84,6 +83,14 @@ TH1D fill_histo(TTree* tree, const char* branch, const char* weight,
   return histo;
 }
 
+template <class T>
+void debug_histo(T histo, Option_t* scale_opt = "") {
+  cout << "Histogram " << histo->GetName() << "has an integral of "
+       << histo->Integral(scale_opt) << endl;
+  cout << "Maximum of " << histo->GetName() << ": " << histo->GetMaximum()
+       << endl;
+}
+
 int main(int, char** argv) {
   TFile* data_file   = new TFile(argv[1], "read");
   TFile* weight_file = new TFile(argv[2], "read");
@@ -98,50 +105,53 @@ int main(int, char** argv) {
   weight_tree->BuildIndex("runNumber", "eventNumber");
   data_tree->AddFriend(weight_tree);
 
-  auto normalization = data_tree->GetEntries();
-
   // Reference CLN
   auto histo_ref_cln_B0ToDstTauNu =
-      q2_histo(BMeson::Neutral, FFType::CLN, m_Tau, normalization, "CLN",
-               "Reference CLN", 80, 2.5, 12);
+      q2_histo(BMeson::Neutral, FFType::CLN, m_Tau, "CLN", "Reference CLN", 200,
+               2.5, 12);
 
   histo_ref_cln_B0ToDstTauNu.SetLineWidth(2);
   histo_ref_cln_B0ToDstTauNu.SetLineColor(kRed);
 
   // Reference ISGW2
   auto histo_ref_isgw2_B0ToDstTauNu =
-      q2_histo(BMeson::Neutral, FFType::ISGW2, m_Tau, normalization, "ISGW2",
-               "Reference ISGW2", 80, 2.5, 12);
-
-  histo_ref_isgw2_B0ToDstTauNu.SetLineWidth(2);
-  histo_ref_isgw2_B0ToDstTauNu.SetLineColor(kBlue);
+      q2_histo(BMeson::Neutral, FFType::ISGW2, m_Tau, "ISGW2",
+               "Reference ISGW2", 200, 2.5, 12);
 
   // Original ISGW2
   auto histo_orig =
-      fill_histo(data_tree, "q2", "q2_orig", "q2 original", 80, 2.5, 12);
-  histo_orig.Scale(1 / histo_orig.Integral("width"));
+      fill_histo(data_tree, "q2", "q2_orig", "q2 original", 70, 2.5, 12);
+  histo_orig.Scale(1 / histo_orig.Integral());
+  debug_histo(&histo_orig);
+
+  // We want to align the maximum of the real data points with its reference
+  // distribution
+  histo_orig.Scale(histo_ref_isgw2_B0ToDstTauNu.GetMaximum() /
+                   histo_orig.GetMaximum());
 
   histo_orig.SetLineWidth(4);
-  histo_orig.SetLineColor(kMagenta);
+  histo_orig.SetLineColor(kGreen);
 
   // Reweighted CLN
   auto histo_reweighted = fill_histo(data_tree, "q2", "w_ff", "q2_reweighted",
-                                     "q2 reweighted", 80, 2.5, 12);
-  histo_reweighted.Scale(1 / histo_reweighted.Integral("width"));
+                                     "q2 reweighted", 70, 2.5, 12);
+  histo_reweighted.Scale(1 / histo_reweighted.Integral());
+  debug_histo(&histo_reweighted);
 
   histo_reweighted.SetLineWidth(4);
   histo_reweighted.SetLineColor(kOrange);
 
-  // Original plot
+  histo_ref_isgw2_B0ToDstTauNu.SetLineWidth(2);
+  histo_ref_isgw2_B0ToDstTauNu.SetLineColor(kBlue);
+
+  // Plot
   auto canvas = new TCanvas("canvas", "FF validation", 4000, 3000);
   histo_ref_cln_B0ToDstTauNu.Draw("hist C");
   histo_ref_isgw2_B0ToDstTauNu.Draw("same hist C");
   histo_orig.Draw("same hist");
   histo_reweighted.Draw("same hist");
 
-  // Legend
   canvas->BuildLegend();
-
   canvas->Update();
   canvas->Print((output_dir + "/validate_ff.png").c_str());
 
