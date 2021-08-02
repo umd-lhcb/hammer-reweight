d910398b48f7e2214b09c662e02fb5edf53e0529
Yipeng Sun <syp@umd.edu>
2021-08-02 23:39:11 +0200

    Define the reweighting skeleton.

---
+++ src/ReweightRDX.cpp
@@ -0,0 +1,358 @@
+// Author: Yipeng Sun
+// Last Change: Mon Aug 02, 2021 at 11:38 PM +0200
+
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <string>
+#include <string_view>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+#include <TFile.h>
+#include <TMath.h>
+#include <TString.h>
+#include <TTree.h>
+#include <TTreeReader.h>
+
+#include <Hammer/Hammer.hh>
+#include <Hammer/Math/FourMomentum.hh>
+#include <Hammer/Particle.hh>
+#include <Hammer/Process.hh>
+#include <Hammer/Tools/HammerRoot.hh>
+
+using namespace std;
+
+///////////////////
+// Configurables //
+///////////////////
+
+typedef map<vector<Int_t>, unsigned long> DecayFreq;
+
+// clang-format off
+auto B_MESON = map<TString, TString>{
+  {"TupleBminus/DecayTree", "b"},
+  {"TupleB0/DecayTree", "b0"}
+};
+
+const auto LEGAL_B_MESON_IDS = vector<Int_t>{511, 521};
+// clang-format on
+
+const Double_t Q2_MIN = 100. * 100.;
+
+void set_input_ff(Hammer::Hammer& ham, string decay_mode) {
+  ;
+  ;
+}
+
+void set_output_ff(Hammer::Hammer& ham) {
+  ;
+  ;
+}
+
+/////////////////////
+// General helpers //
+/////////////////////
+
+template <template <typename, typename> class Iterable, typename T,
+          typename Allocator>
+Bool_t find_in(Iterable<T, Allocator> iter, T elem) {
+  if (find(iter.begin(), iter.end(), elem) != iter.end()) return true;
+  return false;
+}
+
+vector<string> split(const string& s, char delim) {
+  stringstream   ss(s);
+  string         item;
+  vector<string> elems;
+
+  while (getline(ss, item, delim)) {
+    elems.push_back(move(item));
+  }
+
+  return elems;
+}
+
+TString dirname(string s) {
+  auto splitted = split(s, '/');
+  if (splitted.size() == 1) return TString("");
+
+  TString dir = "";
+  for (int idx = 0; idx < splitted.size() - 1; idx++) {
+    dir += splitted[idx];
+    if (idx < splitted.size() - 2) dir += "/";
+  }
+
+  return dir;
+}
+
+TString basename(string s) { return TString(split(s, '/').back()); }
+
+/////////////////////////
+// Particle ID helpers //
+/////////////////////////
+
+Bool_t is_d_meson(Int_t id) {
+  id = TMath::Abs(id);
+
+  return false;
+}
+
+////////////////////////////
+// HAMMER-related helpers //
+////////////////////////////
+
+auto particle(Double_t pe, Double_t px, Double_t py, Double_t pz, Int_t pid) {
+  auto four_mom = Hammer::FourMomentum(pe, px, py, pz);
+  auto part_id  = static_cast<Hammer::PdgId>(pid);
+
+  return Hammer::Particle(four_mom, part_id);
+}
+
+/////////////////
+// Reweighting //
+/////////////////
+
+typedef pair<unsigned long, unsigned long> RwRate;
+
+RwRate reweight(TFile* input_ntp, TFile* output_ntp, TString tree) {
+  if (B_MESON.find(tree) == B_MESON.end()) return RwRate{0, 0};
+  TString b_meson = B_MESON[tree];
+
+  // Input /////////////////////////////////////////////////////////////////////
+  TTreeReader reader(tree, input_ntp);
+
+  // B meson truth info
+  TTreeReaderValue<Double_t> b_px(reader, b_meson + "_TRUEP_X");
+  TTreeReaderValue<Double_t> b_py(reader, b_meson + "_TRUEP_Y");
+  TTreeReaderValue<Double_t> b_pz(reader, b_meson + "_TRUEP_Z");
+  TTreeReaderValue<Double_t> b_pe(reader, b_meson + "_TRUEP_E");
+  TTreeReaderValue<Int_t>    b_id(reader, b_meson + "_TRUEID");
+  TTreeReaderValue<Double_t> q2(reader, b_meson + "_True_Q2");
+
+  // Muon
+  TTreeReaderValue<Double_t> mu_px(reader, b_meson + "_TrueMu_PX");
+  TTreeReaderValue<Double_t> mu_py(reader, b_meson + "_TrueMu_PY");
+  TTreeReaderValue<Double_t> mu_pz(reader, b_meson + "_TrueMu_PZ");
+  TTreeReaderValue<Double_t> mu_pe(reader, b_meson + "_TrueMu_PE");
+
+  // Tau
+  TTreeReaderValue<Double_t> tau_px(reader, b_meson + "_TrueTau_PX");
+  TTreeReaderValue<Double_t> tau_py(reader, b_meson + "_TrueTau_PY");
+  TTreeReaderValue<Double_t> tau_pz(reader, b_meson + "_TrueTau_PZ");
+  TTreeReaderValue<Double_t> tau_pe(reader, b_meson + "_TrueTau_PE");
+
+  // The neutrino that is directly associated with the B -> D weak decay
+  // Could be a Mu or Tau anti-neutrino.
+  TTreeReaderValue<Double_t> anu_px(reader, b_meson + "_TrueNeutrino_PX");
+  TTreeReaderValue<Double_t> anu_py(reader, b_meson + "_TrueNeutrino_PY");
+  TTreeReaderValue<Double_t> anu_pz(reader, b_meson + "_TrueNeutrino_PZ");
+  TTreeReaderValue<Double_t> anu_pe(reader, b_meson + "_TrueNeutrino_PE");
+
+  // Tau neutrino and Mu anti-neutrino associated with the secondary tau decay
+  TTreeReaderValue<Double_t> nu_tau_px(reader, b_meson + "_TrueTauNuTau_PX");
+  TTreeReaderValue<Double_t> nu_tau_py(reader, b_meson + "_TrueTauNuTau_PY");
+  TTreeReaderValue<Double_t> nu_tau_pz(reader, b_meson + "_TrueTauNuTau_PZ");
+  TTreeReaderValue<Double_t> nu_tau_pe(reader, b_meson + "_TrueTauNuTau_PE");
+
+  TTreeReaderValue<Double_t> anu_mu_px(reader, b_meson + "_TrueTauNuMu_PX");
+  TTreeReaderValue<Double_t> anu_mu_py(reader, b_meson + "_TrueTauNuMu_PY");
+  TTreeReaderValue<Double_t> anu_mu_pz(reader, b_meson + "_TrueTauNuMu_PZ");
+  TTreeReaderValue<Double_t> anu_mu_pe(reader, b_meson + "_TrueTauNuMu_PE");
+
+  // First D meson and its daughters
+  TTreeReaderValue<Double_t> d_idx0_px(reader, b_meson + "_TrueHadron_D0_PX");
+  TTreeReaderValue<Double_t> d_idx0_py(reader, b_meson + "_TrueHadron_D0_PY");
+  TTreeReaderValue<Double_t> d_idx0_pz(reader, b_meson + "_TrueHadron_D0_PZ");
+  TTreeReaderValue<Double_t> d_idx0_pe(reader, b_meson + "_TrueHadron_D0_PE");
+  TTreeReaderValue<Int_t>    d_idx0_id(reader, b_meson + "_TrueHadron_D0_ID");
+
+  TTreeReaderValue<Double_t> d_idx0_gd0_px(reader,
+                                           b_meson + "_TrueHadron_D0_GD0_PX");
+  TTreeReaderValue<Double_t> d_idx0_gd0_py(reader,
+                                           b_meson + "_TrueHadron_D0_GD0_PY");
+  TTreeReaderValue<Double_t> d_idx0_gd0_pz(reader,
+                                           b_meson + "_TrueHadron_D0_GD0_PZ");
+  TTreeReaderValue<Double_t> d_idx0_gd0_pe(reader,
+                                           b_meson + "_TrueHadron_D0_GD0_PE");
+  TTreeReaderValue<Int_t>    d_idx0_gd0_id(reader,
+                                        b_meson + "_TrueHadron_D0_GD0_ID");
+
+  TTreeReaderValue<Double_t> d_idx0_gd1_px(reader,
+                                           b_meson + "_TrueHadron_D0_GD1_PX");
+  TTreeReaderValue<Double_t> d_idx0_gd1_py(reader,
+                                           b_meson + "_TrueHadron_D0_GD1_PY");
+  TTreeReaderValue<Double_t> d_idx0_gd1_pz(reader,
+                                           b_meson + "_TrueHadron_D0_GD1_PZ");
+  TTreeReaderValue<Double_t> d_idx0_gd1_pe(reader,
+                                           b_meson + "_TrueHadron_D0_GD1_PE");
+  TTreeReaderValue<Int_t>    d_idx0_gd1_id(reader,
+                                        b_meson + "_TrueHadron_D0_GD1_ID");
+
+  TTreeReaderValue<Double_t> d_idx0_gd2_px(reader,
+                                           b_meson + "_TrueHadron_D0_GD2_PX");
+  TTreeReaderValue<Double_t> d_idx0_gd2_py(reader,
+                                           b_meson + "_TrueHadron_D0_GD2_PY");
+  TTreeReaderValue<Double_t> d_idx0_gd2_pz(reader,
+                                           b_meson + "_TrueHadron_D0_GD2_PZ");
+  TTreeReaderValue<Double_t> d_idx0_gd2_pe(reader,
+                                           b_meson + "_TrueHadron_D0_GD2_PE");
+  TTreeReaderValue<Int_t>    d_idx0_gd2_id(reader,
+                                        b_meson + "_TrueHadron_D0_GD2_ID");
+
+  // Second D meson and its daughters
+  TTreeReaderValue<Double_t> d_idx1_px(reader, b_meson + "_TrueHadron_D1_PX");
+  TTreeReaderValue<Double_t> d_idx1_py(reader, b_meson + "_TrueHadron_D1_PY");
+  TTreeReaderValue<Double_t> d_idx1_pz(reader, b_meson + "_TrueHadron_D1_PZ");
+  TTreeReaderValue<Double_t> d_idx1_pe(reader, b_meson + "_TrueHadron_D1_PE");
+  TTreeReaderValue<Int_t>    d_idx1_id(reader, b_meson + "_TrueHadron_D1_ID");
+
+  TTreeReaderValue<Double_t> d_idx1_gd0_px(reader,
+                                           b_meson + "_TrueHadron_D1_GD0_PX");
+  TTreeReaderValue<Double_t> d_idx1_gd0_py(reader,
+                                           b_meson + "_TrueHadron_D1_GD0_PY");
+  TTreeReaderValue<Double_t> d_idx1_gd0_pz(reader,
+                                           b_meson + "_TrueHadron_D1_GD0_PZ");
+  TTreeReaderValue<Double_t> d_idx1_gd0_pe(reader,
+                                           b_meson + "_TrueHadron_D1_GD0_PE");
+  TTreeReaderValue<Int_t>    d_idx1_gd0_id(reader,
+                                        b_meson + "_TrueHadron_D1_GD0_ID");
+
+  TTreeReaderValue<Double_t> d_idx1_gd1_px(reader,
+                                           b_meson + "_TrueHadron_D1_GD1_PX");
+  TTreeReaderValue<Double_t> d_idx1_gd1_py(reader,
+                                           b_meson + "_TrueHadron_D1_GD1_PY");
+  TTreeReaderValue<Double_t> d_idx1_gd1_pz(reader,
+                                           b_meson + "_TrueHadron_D1_GD1_PZ");
+  TTreeReaderValue<Double_t> d_idx1_gd1_pe(reader,
+                                           b_meson + "_TrueHadron_D1_GD1_PE");
+  TTreeReaderValue<Int_t>    d_idx1_gd1_id(reader,
+                                        b_meson + "_TrueHadron_D1_GD1_ID");
+
+  TTreeReaderValue<Double_t> d_idx1_gd2_px(reader,
+                                           b_meson + "_TrueHadron_D1_GD2_PX");
+  TTreeReaderValue<Double_t> d_idx1_gd2_py(reader,
+                                           b_meson + "_TrueHadron_D1_GD2_PY");
+  TTreeReaderValue<Double_t> d_idx1_gd2_pz(reader,
+                                           b_meson + "_TrueHadron_D1_GD2_PZ");
+  TTreeReaderValue<Double_t> d_idx1_gd2_pe(reader,
+                                           b_meson + "_TrueHadron_D1_GD2_PE");
+  TTreeReaderValue<Int_t>    d_idx1_gd2_id(reader,
+                                        b_meson + "_TrueHadron_D1_GD2_ID");
+
+  // Third D meson and its daughters
+  TTreeReaderValue<Double_t> d_idx2_px(reader, b_meson + "_TrueHadron_D2_PX");
+  TTreeReaderValue<Double_t> d_idx2_py(reader, b_meson + "_TrueHadron_D2_PY");
+  TTreeReaderValue<Double_t> d_idx2_pz(reader, b_meson + "_TrueHadron_D2_PZ");
+  TTreeReaderValue<Double_t> d_idx2_pe(reader, b_meson + "_TrueHadron_D2_PE");
+  TTreeReaderValue<Int_t>    d_idx2_id(reader, b_meson + "_TrueHadron_D2_ID");
+
+  TTreeReaderValue<Double_t> d_idx2_gd0_px(reader,
+                                           b_meson + "_TrueHadron_D2_GD0_PX");
+  TTreeReaderValue<Double_t> d_idx2_gd0_py(reader,
+                                           b_meson + "_TrueHadron_D2_GD0_PY");
+  TTreeReaderValue<Double_t> d_idx2_gd0_pz(reader,
+                                           b_meson + "_TrueHadron_D2_GD0_PZ");
+  TTreeReaderValue<Double_t> d_idx2_gd0_pe(reader,
+                                           b_meson + "_TrueHadron_D2_GD0_PE");
+  TTreeReaderValue<Int_t>    d_idx2_gd0_id(reader,
+                                        b_meson + "_TrueHadron_D2_GD0_ID");
+
+  TTreeReaderValue<Double_t> d_idx2_gd1_px(reader,
+                                           b_meson + "_TrueHadron_D2_GD1_PX");
+  TTreeReaderValue<Double_t> d_idx2_gd1_py(reader,
+                                           b_meson + "_TrueHadron_D2_GD1_PY");
+  TTreeReaderValue<Double_t> d_idx2_gd1_pz(reader,
+                                           b_meson + "_TrueHadron_D2_GD1_PZ");
+  TTreeReaderValue<Double_t> d_idx2_gd1_pe(reader,
+                                           b_meson + "_TrueHadron_D2_GD1_PE");
+  TTreeReaderValue<Int_t>    d_idx2_gd1_id(reader,
+                                        b_meson + "_TrueHadron_D2_GD1_ID");
+
+  TTreeReaderValue<Double_t> d_idx2_gd2_px(reader,
+                                           b_meson + "_TrueHadron_D2_GD2_PX");
+  TTreeReaderValue<Double_t> d_idx2_gd2_py(reader,
+                                           b_meson + "_TrueHadron_D2_GD2_PY");
+  TTreeReaderValue<Double_t> d_idx2_gd2_pz(reader,
+                                           b_meson + "_TrueHadron_D2_GD2_PZ");
+  TTreeReaderValue<Double_t> d_idx2_gd2_pe(reader,
+                                           b_meson + "_TrueHadron_D2_GD2_PE");
+  TTreeReaderValue<Int_t>    d_idx2_gd2_id(reader,
+                                        b_meson + "_TrueHadron_D2_GD2_ID");
+
+  // Output ////////////////////////////////////////////////////////////////////
+  // Recreate the same folder structure in output
+  auto tree_dir  = dirname(string(tree));
+  auto tree_name = basename(string(tree));
+
+  output_ntp->cd();
+  if (tree_dir != TString("")) {
+    output_ntp->mkdir(tree_dir);
+    output_ntp->cd(tree_dir);
+  }
+
+  // Output branches
+  auto output_tree = new TTree(tree_name, tree_name);
+
+  Bool_t ham_ok;
+  output_tree->Branch("flag_ham_ok", &ham_ok);
+
+  ULong64_t eventNumber_out;
+  output_tree->Branch("eventNumber", &eventNumber_out);
+  UInt_t runNumber_out;
+  output_tree->Branch("runNumber", &runNumber_out);
+
+  // Setup HAMMER //////////////////////////////////////////////////////////////
+  Hammer::Hammer   ham{};
+  Hammer::IOBuffer ham_buf;
+
+  ham.setUnits("MeV");
+  ham.initRun();
+
+  unsigned long num_of_evt           = 0l;
+  unsigned long num_of_evt_w_b_meson = 0l;
+  while (reader.Next()) {
+    ham_ok = false;
+
+    // Check if we have a legal B meson and q2 is large enough to produce a Mu
+    if (find_in(LEGAL_B_MESON_IDS, TMath::Abs(*b_id)) && *q2 > Q2_MIN) {
+      // Check if we have a legal D meson
+      auto d_meson_cands = map<TString, Int_t>{
+          {"D0", *d_idx0_id}, {"D1", *d_idx1_id}, {"D2", *d_idx2_id}};
+      if (find_in()) {
+        // If so, locate the right D meson and loop over all its daughters and
+        // add them if they are hadrons
+        num_of_evt_w_b_meson += 1;
+      }
+    }
+
+    num_of_evt += 1;
+  }
+
+  // Cleanup ///////////////////////////////////////////////////////////////////
+  delete output_tree;
+
+  return RwRate{num_of_evt, num_of_evt_w_b_meson};
+}
+
+//////////
+// Main //
+//////////
+
+int main(int, char** argv) {
+  auto    input_ntp  = new TFile(argv[1], "read");
+  auto    output_ntp = new TFile(argv[2], "recreate");
+  TString tree       = argv[3];
+
+  auto rate = reweight(input_ntp, output_ntp, tree);
+
+  cout << "Total number of candidates: " << get<0>(rate) << endl;
+  cout << "Truth-matched candidates: " << get<1>(rate) << endl;
+  cout << "Truth-matched fraction: "
+       << static_cast<float>(get<0>(rate)) / static_cast<float>(get<1>(rate))
+       << endl;
+
+  delete input_ntp;
+  delete output_ntp;
+}
