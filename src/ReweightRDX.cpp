db8e179c92fb8527be9901d49b882f9272f66771
Yipeng Sun <syp@umd.edu>
2021-08-04 16:15:06 +0200

    Partially add particles.

--- src/ReweightRDX.cpp
+++ src/ReweightRDX.cpp
@@ -1,5 +1,5 @@
 // Author: Yipeng Sun
-// Last Change: Tue Aug 03, 2021 at 02:30 AM +0200
+// Last Change: Tue Aug 03, 2021 at 08:41 PM +0200
 
 #include <algorithm>
 #include <iostream>
@@ -46,7 +46,7 @@ void set_input_ff(Hammer::Hammer& ham, string decay_mode) {
   ;
 }
 
-void set_output_ff(Hammer::Hammer& ham) {
+void set_output_ff(Hammer::Hammer& ham, string decay_mode) {
   ;
   ;
 }
@@ -90,23 +90,35 @@ TString dirname(string s) {
 TString basename(string s) { return TString(split(s, '/').back()); }
 
 Int_t digit_is(Int_t num, Int_t digit, Int_t base = 10) {
-  return num / (digit * base);
+  Int_t fac = num / TMath::Power(base, digit - 1);
+  return fac % 10;
 }
 
 /////////////////////////
 // Particle ID helpers //
 /////////////////////////
 
-typedef pair<Bool_t, TString> DMesonPack;
-typedef map<TString, Int_t>   PartIdMap;
+typedef pair<Bool_t, TString>  DMesonPack;
+typedef map<TString, Int_t>    PartIdMap;
+typedef map<TString, Double_t> PartMomMap;
 
-DMesonPack is_d_meson(const PartIdMap parts) {
+DMesonPack is_D_meson(const PartIdMap parts) {
   for (const auto [key, val] : parts) {
     auto id = TMath::Abs(val);
     if (digit_is(id, 3) == 4) return DMesonPack{true, key};
   }
 
   return DMesonPack{false, "none"};
+}
+
+Bool_t is_hadron(const Int_t id) {
+  if (TMath::Abs(id) > 100) return true;
+  return false;
+}
+
+Int_t B_id_fix(const Int_t B_id, const Int_t D_id) {
+  if (B_id * D_id > 0) return -B_id;
+  return B_id;
 }
 
 ////////////////////////////
@@ -328,13 +340,117 @@ RwRate reweight(TFile* input_ntp, TFile* output_ntp, TString tree) {
     // Check if we have a legal B meson and q2 is large enough to produce a Mu
     if (find_in(LEGAL_B_MESON_IDS, TMath::Abs(*b_id)) && *q2 > Q2_MIN) {
       // Check if we have a legal D meson
-      auto d_meson_cands =
+      // clang-format off
+      auto D_cands =
           PartIdMap{{"D0", *d_idx0_id}, {"D1", *d_idx1_id}, {"D2", *d_idx2_id}};
-      auto [d_meson_ok, d_meson_lbl] = is_d_meson(d_meson_cands);
+      auto D_mom = PartMomMap{
+          {"D0_PX", *d_idx0_px},
+          {"D0_PY", *d_idx0_py},
+          {"D0_PZ", *d_idx0_pz},
+          {"D0_PE", *d_idx0_pe},
 
-      if (d_meson_ok) {
+          {"D1_PX", *d_idx1_px},
+          {"D1_PY", *d_idx1_py},
+          {"D1_PZ", *d_idx1_pz},
+          {"D1_PE", *d_idx1_pe},
+
+          {"D2_PX", *d_idx2_px},
+          {"D2_PY", *d_idx2_py},
+          {"D2_PZ", *d_idx2_pz},
+          {"D2_PE", *d_idx2_pe}
+      };
+      // clang-format on
+      auto [D_ok, D_lbl] = is_D_meson(D_cands);
+
+      if (D_ok) {
+        // clang-format off
+        auto D_daughter_id = PartIdMap{
+          {"D0_GD0", *d_idx0_gd0_id},
+          {"D0_GD1", *d_idx0_gd1_id},
+          {"D0_GD2", *d_idx0_gd2_id},
+
+          {"D1_GD0", *d_idx1_gd0_id},
+          {"D1_GD1", *d_idx1_gd1_id},
+          {"D1_GD2", *d_idx1_gd2_id},
+
+          {"D2_GD0", *d_idx2_gd0_id},
+          {"D2_GD1", *d_idx2_gd1_id},
+          {"D2_GD2", *d_idx2_gd2_id}
+        };
+
+        auto D_daughter_mom = PartMomMap{
+          {"D0_GD0_PX", *d_idx0_gd0_px},
+          {"D0_GD0_PY", *d_idx0_gd0_py},
+          {"D0_GD0_PZ", *d_idx0_gd0_pz},
+          {"D0_GD0_PE", *d_idx0_gd0_pe},
+
+          {"D0_GD1_PX", *d_idx0_gd1_px},
+          {"D0_GD1_PY", *d_idx0_gd1_py},
+          {"D0_GD1_PZ", *d_idx0_gd1_pz},
+          {"D0_GD1_PE", *d_idx0_gd1_pe},
+
+          {"D0_GD2_PX", *d_idx0_gd2_px},
+          {"D0_GD2_PY", *d_idx0_gd2_py},
+          {"D0_GD2_PZ", *d_idx0_gd2_pz},
+          {"D0_GD2_PE", *d_idx0_gd2_pe},
+
+          {"D1_GD0_PX", *d_idx1_gd0_px},
+          {"D1_GD0_PY", *d_idx1_gd0_py},
+          {"D1_GD0_PZ", *d_idx1_gd0_pz},
+          {"D1_GD0_PE", *d_idx1_gd0_pe},
+
+          {"D1_GD1_PX", *d_idx1_gd1_px},
+          {"D1_GD1_PY", *d_idx1_gd1_py},
+          {"D1_GD1_PZ", *d_idx1_gd1_pz},
+          {"D1_GD1_PE", *d_idx1_gd1_pe},
+
+          {"D1_GD2_PX", *d_idx1_gd2_px},
+          {"D1_GD2_PY", *d_idx1_gd2_py},
+          {"D1_GD2_PZ", *d_idx1_gd2_pz},
+          {"D1_GD2_PE", *d_idx1_gd2_pe},
+
+          {"D2_GD0_PX", *d_idx2_gd0_px},
+          {"D2_GD0_PY", *d_idx2_gd0_py},
+          {"D2_GD0_PZ", *d_idx2_gd0_pz},
+          {"D2_GD0_PE", *d_idx2_gd0_pe},
+
+          {"D2_GD1_PX", *d_idx2_gd1_px},
+          {"D2_GD1_PY", *d_idx2_gd1_py},
+          {"D2_GD1_PZ", *d_idx2_gd1_pz},
+          {"D2_GD1_PE", *d_idx2_gd1_pe},
+
+          {"D2_GD2_PX", *d_idx2_gd2_px},
+          {"D2_GD2_PY", *d_idx2_gd2_py},
+          {"D2_GD2_PZ", *d_idx2_gd2_pz},
+          {"D2_GD2_PE", *d_idx2_gd2_pe}
+        };
+        // clang-format on
+
         // If so, locate the right D meson and loop over all its daughters and
         // add them if they are hadrons
+        vector<Hammer::Particle> d_daughters{};
+
+        for (auto suffix : vector<TString>{"_GD0", "_GD1", "_GD2"}) {
+          auto part_name = D_lbl + suffix;
+          auto part_id   = D_daughter_id[part_name];
+          if (is_hadron(part_id)) {
+            d_daughters.push_back(particle(D_daughter_mom[part_name + "PE"],
+                                           D_daughter_mom[part_name + "PX"],
+                                           D_daughter_mom[part_name + "PY"],
+                                           D_daughter_mom[part_name + "PZ"],
+                                           part_id));
+          }
+        }
+
+        // HAMMER process
+        Hammer::Process proc;
+
+        auto part_B = particle(*b_pe, *b_px, *b_py, *b_pz,
+                               B_id_fix(*b_id, D_cands[D_lbl]));
+        auto part_D = particle(D_mom[D_lbl + "_PX"], D_mom[D_lbl + "_PY"],
+                               D_mom[D_lbl + "_PZ"], D_mom[D_lbl + "_PE"],
+                               D_cands[D_lbl]);
+
         num_of_evt_w_b_meson += 1;
       }
     }
