055a92aaf2807dc0d24eae884d88669947e8e217
Yipeng Sun <syp@umd.edu>
2021-08-05 20:35:17 +0200

    Provide a Python virtualenv.

--- flake.nix
+++ flake.nix
@@ -17,22 +17,49 @@
           inherit system;
           overlays = [ root-curated.overlay self.overlay ];
         };
+        python = pkgs.python3;
+        pythonPackages = python.pkgs;
       in
       rec {
         packages = flake-utils.lib.flattenTree {
           dev-shell = devShell.inputDerivation;
         };
-        devShell = pkgs.mkShell {
+        devShell = pkgs.mkShell rec {
           name = "hammer-reweight";
-          buildInputs = with pkgs; [
+          buildInputs = (with pkgs; with pythonPackages; [
             root
             hammer-phys
             ff_calc
-            python3
             clang-tools
-          ];
+
+            # Python virtualenv
+            jedi
+            flake8
+            pylint
+            virtualenvwrapper
+          ]);
 
           shellHook = ''
+            # Allow the use of wheels.
+            SOURCE_DATE_EPOCH=$(date +%s)
+
+            if test -d $HOME/build/python-venv; then
+              VENV=$HOME/build/python-venv/${name}
+            else
+              VENV=./.virtualenv
+            fi
+
+            if test ! -d $VENV; then
+              virtualenv $VENV
+            fi
+            source $VENV/bin/activate
+
+            # allow for the environment to pick up packages installed with virtualenv
+            export PYTHONPATH=$VENV/${python.sitePackages}/:$PYTHONPATH
+
+            # fix libstdc++.so not found error
+            export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH
+
             export PATH=$(pwd)/bin:$(pwd)/utils:$PATH
           '';
         };
