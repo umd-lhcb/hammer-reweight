4d2d3e81707a3e06005271d8a077e47e703dfeca
Yipeng Sun <syp@umd.edu>
2021-08-13 01:42:43 +0200

    Update plotter.

--- utils/plot_ratio.py
+++ utils/plot_ratio.py
@@ -4,11 +4,14 @@
 # License: GPLv2
 # Based on:
 #   https://github.com/ZishuoYang/my-hammer-reweighting/blob/master/plot_ratio.py
-# Last Change: Thu Aug 05, 2021 at 08:51 PM +0200
+# Last Change: Fri Aug 13, 2021 at 01:42 AM +0200
 
 import ROOT as rt
+rt.PyConfig.IgnoreCommandLineOptions = True  # Don't hijack argparse!
+rt.PyConfig.DisableRootLogon = True  # Don't read .rootlogon.py
 
 from argparse import ArgumentParser
+from os.path import basename, splitext
 
 
 #################################
@@ -19,26 +22,22 @@ def parse_input():
     parser = ArgumentParser(description='''
 Plot fit variables of R(D(*)) with and without FF reweighting.''')
 
+    parser.add_argument('-n', '--ntuple',
+                        required=True,
+                        help='''
+specify path to ntuple that contains HAMMER weights.''')
+
+    parser.add_argument('-t', '--tree',
+                        required=True,
+                        help='''
+specify tree name in HAMMER-weight ntuple.''')
+
     parser.add_argument('-o', '--output-path',
-                        nargs='?',
                         default='./gen',
                         help='''
 specify output path for plots.''')
 
-    parser.add_argument('-d', '--data-ntuple',
-                        nargs='?',
-                        required=True,
-                        help='''
-specify path to ntuple that contains fit variables.''')
-
-    parser.add_argument('-t', '--data-tree',
-                        nargs='?',
-                        required=True,
-                        help='''
-specify tree name in fit-variable ntuple.''')
-
     parser.add_argument('--ff-weight',
-                        nargs='?',
                         default='w_ff',
                         help='''
 specify branch name of the FF weight.''')
@@ -56,7 +55,7 @@ specify variables to plot.''')
 
     parser.add_argument('--bin-ranges',
                         nargs='+',
-                        default=['(80,3,11)', '(80,-0.2,10)', '(80,0.,2.1)'],
+                        default=['(15,0,12)', '(15,-0.2,10)', '(15,0.,2.1)'],
                         help='''
 specify number of bins and x ranges.''')
 
@@ -67,19 +66,21 @@ specify number of bins and x ranges.''')
 # Helpers #
 ###########
 
-def plot_ratio(tree, output_path,
-               var, weight, title,
-               bin_range, cuts):
-    # rt.gStyle.SetOptStat(0)
-    canvas = rt.TCanvas('canvas', 'A ratio plot')
+def get_filename(path):
+    return basename(splitext(path)[0])
 
-    tree.Draw('{}>>h1{}'.format(var, bin_range), cuts, 'goff')
+
+def plot_ratio(tree, output_path, ntp_name,
+               var, bin_range, cuts, weight):
+    canvas = rt.TCanvas('canvas', 'canvas')
+    plot_suffix = get_filename(ntp_name)
+
+    tree.Draw('{}>>h1{}'.format(var, bin_range), cuts, "goff")
     h1 = rt.gDirectory.Get('h1')
     h1.SetMarkerColor(rt.kBlue)
     h1.SetLineColor(rt.kBlue)
 
-    tree.Draw('{}>>h2{}'.format(var, bin_range), '({})*{}'.format(cuts, weight)
-              , 'goff')
+    tree.Draw('{}>>h2{}'.format(var, bin_range), '{}'.format(weight), "goff")
     h2 = rt.gDirectory.Get('h2')
     h2.SetMarkerColor(rt.kRed)
     h2.SetLineColor(rt.kRed)
@@ -88,19 +89,17 @@ def plot_ratio(tree, output_path,
     rp.Draw()
 
     canvas.Update()
-
-    canvas.Print('{}/{}.png'.format(output_path, title))
+    canvas.Print('{}/{}_{}.png'.format(output_path, plot_suffix, var))
 
 
 if __name__ == '__main__':
     args = parse_input()
 
-    data_ntuple = rt.TFile(args.data_ntuple)
-    data_tree = data_ntuple.Get(args.data_tree)
+    ntuple = rt.TFile(args.ntuple)
+    tree = ntuple.Get(args.tree)
 
     rt.gROOT.SetBatch(rt.kTRUE)  # Don't output anything on screen
 
     for var, bin_range in zip(args.vars, args.bin_ranges):
-        plot_ratio(data_tree, args.output_path,
-                   var, args.ff_weight, var,
-                   bin_range, args.cuts)
+        plot_ratio(tree, args.output_path, args.ntuple,
+                   var, bin_range, args.cuts, args.ff_weight)
