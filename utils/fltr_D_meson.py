196ca29c10620747e89efe4dcb18c69468727cb4
Yipeng Sun <syp@umd.edu>
2022-08-17 05:26:16 -0400

    Even better printout; reformat.

--- utils/fltr_D_meson.py
+++ utils/fltr_D_meson.py
@@ -11,34 +11,55 @@ from pyTuplingUtils.io import read_branches, read_branches_dict
 
 
 def parse_input():
-    parser = ArgumentParser(
-        description='filter particles and print its truth info.')
+    parser = ArgumentParser(description="filter particles and print its truth info.")
 
-    parser.add_argument('ntp', help='input ntuple.')
+    parser.add_argument("ntp", help="input ntuple.")
 
-    parser.add_argument('-t', '--tree', default='TupleB0/DecayTree',
-                        help='specify tree name.')
+    parser.add_argument(
+        "-t", "--tree", default="TupleB0/DecayTree", help="specify tree name."
+    )
 
-    parser.add_argument('-i', '--id', nargs='+', type=int,
-                        default=[10421, 10411],
-                        help='specify particle true ID.')
+    parser.add_argument(
+        "-i",
+        "--id",
+        nargs="+",
+        type=int,
+        default=[10421, 10411],
+        help="specify particle true ID.",
+    )
 
-    parser.add_argument('-b', '--base-br', default='d_meson1_true_id',
-                        help='specify the true ID branch.')
+    parser.add_argument(
+        "-b",
+        "--base-br",
+        default="d_meson1_true_id",
+        help="specify the true ID branch.",
+    )
 
-    parser.add_argument('-w', '--weight-br', default='wff',
-                        help='specify the weight branch.')
+    parser.add_argument(
+        "-w", "--weight-br", default="wff", help="specify the weight branch."
+    )
 
-    parser.add_argument('-o', '--ok-br', default='ham_ok',
-                        help='specify the cut branch.')
+    parser.add_argument(
+        "-o", "--ok-br", default="ham_ok", help="specify the cut branch."
+    )
 
-    parser.add_argument('-B', '--add-br', nargs='+', default=[
-        'd_meson1_true_m',
-    ], help='specify additional branches.')
+    parser.add_argument(
+        "-B",
+        "--add-br",
+        nargs="+",
+        default=[
+            "d_meson1_true_m",
+        ],
+        help="specify additional branches.",
+    )
 
-    parser.add_argument('-N', '--name', nargs='+', default=[
-        'inv.mass.'
-    ], help='specify printout names of the additional branches.')
+    parser.add_argument(
+        "-N",
+        "--name",
+        nargs="+",
+        default=["inv.mass."],
+        help="specify printout names of the additional branches.",
+    )
 
     return parser.parse_args()
 
@@ -53,16 +74,20 @@ def fltr_func(true_ids):
     return np.vectorize(inner)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = parse_input()
     ntp = uproot.open(args.ntp)
 
     br_id, br_wt, br_ok = read_branches(
-        ntp, args.tree, [args.base_br, args.weight_br, args.ok_br])
+        ntp, args.tree, [args.base_br, args.weight_br, args.ok_br]
+    )
     br_addon = read_branches_dict(ntp, args.tree, args.add_br)
 
-    print('Keep particles with these true IDs: {}'.format(
-        ', '.join([str(i) for i in args.id])))
+    print(
+        "Keep particles with these true IDs: {}".format(
+            ", ".join([str(i) for i in args.id])
+        )
+    )
     fltr = fltr_func(args.id)
     sel = fltr(br_id)
 
@@ -82,6 +107,6 @@ if __name__ == '__main__':
         if not br_ok[idx]:
             continue
 
-        print('ID: {}, Weight: {:.3f}'.format(br_id[idx], br_wt[idx]))
+        print("ID: {}, Weight: {:.3f}".format(br_id[idx], br_wt[idx]))
         for key in br_addon:
-            print('  {}: {:.3f}'.format(name_dict[key], br_addon[key][idx]))
+            print("  {}: {:.3f}".format(name_dict[key], br_addon[key][idx]))
--- utils/gen_ham_params.py
+++ utils/gen_ham_params.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #
 # Author: Yipeng Sun, Alex Fernez
-# Last Change: Sun Aug 14, 2022 at 05:45 PM -0400
+# Last Change: Wed Aug 17, 2022 at 05:25 AM -0400
 
 import yaml
 import numpy as np
@@ -13,69 +13,87 @@ from argparse import ArgumentParser
 # Command line parser #
 #######################
 
+
 def parse_input():
-    parser = ArgumentParser(
-        description='generate HAMMER parameters.')
+    parser = ArgumentParser(description="generate HAMMER parameters.")
 
-    parser.add_argument('input', help='specify input YAML.')
+    parser.add_argument("input", help="specify input YAML.")
 
-    parser.add_argument('-m', '--modes', nargs='+',
-                        default=['BtoDBGLVar', 'BtoDstarBGLVar', 'BtoD0starBLRVar',
-                                 'BtoD1starBLRVar', 'BtoD1BLRVar', 'BtoD2starBLRVar'],
-                        help='specify FF params to print')
+    parser.add_argument(
+        "-m",
+        "--modes",
+        nargs="+",
+        default=[
+            "BtoDBGLVar",
+            "BtoDstarBGLVar",
+            "BtoD0starBLRVar",
+            "BtoD1starBLRVar",
+            "BtoD1BLRVar",
+            "BtoD2starBLRVar",
+        ],
+        help="specify FF params to print",
+    )
 
     return parser.parse_args()
+
 
 ###########
 # Helpers #
 ###########
 
-def print_param_general(ff_alias, param, val):
-    print(f'    "{ff_alias}: {{{param}: {val}}}",')
+
+def print_param_general(ff_alias, param, val, add_hammer=False):
+    if add_hammer:
+        print(f'  ham.setOptions(scheme + ": {{{param}: {val}}}");')
+    else:
+        print(f'    "{ff_alias}: {{{param}: {val}}}",')
 
 
 def print_param_ff_var(process, model, shifts, params, comments):
-    print('  FF variation params:')
+    print("  FF variation params:")
     for single, c in zip(shifts, comments):
         eigen_vector_spec = [f'{{"{p}", {s}}}' for p, s in zip(params, single)]
-        eigen_vector_str = '{' + ', '.join(eigen_vector_spec) + '}'
+        eigen_vector_str = "{" + ", ".join(eigen_vector_spec) + "}"
 
-        print(f'    ham.setFFEigenvectors{{"{process}", "{model}", {eigen_vector_str}}}; // {c};')
+        print(
+            f'    ham.setFFEigenvectors{{"{process}", "{model}", {eigen_vector_str}}}; // {c};'
+        )
 
 
 def fmt_dict_as_cpp_map(dct):
-    output = '{'
+    output = "{"
     for key, value in dct.items():
         elem = f'{{"{key}", {value}}}, '
         output += elem
-    return output[:-2] + '},'
+    return output[:-2] + "},"
 
 
 def eval_fake_sandbox(code, add_vars):
     loc = dict()
     sandbox = {k: v for k, v in globals().items()}
     sandbox.update(add_vars)
-    exec(f'__result = {code}', sandbox, loc)
-    return loc['__result']
+    exec(f"__result = {code}", sandbox, loc)
+    return loc["__result"]
 
 
 #################
 # Model helpers #
 #################
 
+
 def gen_param_var(process, model, m_corr, v_err, param_names, add_params):
     m_corr = np.matrix(m_corr)
     v_err = np.array(v_err)
 
     # make the covariance matrix based on the correlation and errors
-    m_cov = np.einsum('ij,i,j->ij', m_corr, v_err, v_err)
+    m_cov = np.einsum("ij,i,j->ij", m_corr, v_err, v_err)
     v_eigen, m_eigen = np.linalg.eig(m_cov)
     # m_eigen is the matrix formed by eigen vectors; v_eigen is the vector
     #   formed by eigenvalues
     # m_eigen is essentially M in the ANA
     # now we need to find C in the ANA to construct A
-    m_c = np.einsum('ij,i->ij', np.eye(v_eigen.size), (1 / np.sqrt(v_eigen)))
-    m_a = np.einsum('ik,kj', m_c, m_eigen)
+    m_c = np.einsum("ij,i->ij", np.eye(v_eigen.size), (1 / np.sqrt(v_eigen)))
+    m_a = np.einsum("ik,kj", m_c, m_eigen)
     # NOTE: uncomment the line below to reproduce RD+'s numbers
     m_a = m_eigen
     # now find the inverse of A, A^-1 tells us how to transform from
@@ -87,160 +105,175 @@ def gen_param_var(process, model, m_corr, v_err, param_names, add_params):
     # ERROR eigen vector
     # the output will be: u1p, u1m, u2p, u2m, ...
     print()
-    print(f'{process}{model} FF variations:')
+    print(f"{process}{model} FF variations:")
     for i in range(len(param_names)):
-        var_values = m_a_inv[:,i]
-        var_pos = {'delta_'+k: v for k, v in zip(param_names, var_values)}
+        var_values = m_a_inv[:, i]
+        var_pos = {"delta_" + k: v for k, v in zip(param_names, var_values)}
         for name, coeff in add_params.items():
-            var_pos['delta_'+name] = np.dot(np.array(coeff), var_values)
+            var_pos["delta_" + name] = np.dot(np.array(coeff), var_values)
 
         var_neg = {k: -v for k, v in var_pos.items()}
-        print(f'  {fmt_dict_as_cpp_map(var_pos)}')
-        print(f'  {fmt_dict_as_cpp_map(var_neg)}')
+        print(f"  {fmt_dict_as_cpp_map(var_pos)}")
+        print(f"  {fmt_dict_as_cpp_map(var_neg)}")
 
 
 def gen_param_shifted_BtoDBGL(
-        process, model, m_corr, v_err, add_params, ap, a0, verbose=True):
+    process, model, m_corr, v_err, add_params, ap, a0, verbose=True
+):
     m_corr = np.matrix(m_corr)
     v_err = np.array(v_err)
 
-    m_cov = np.einsum('ij,i,j->ij', m_corr, v_err, v_err)
+    m_cov = np.einsum("ij,i,j->ij", m_corr, v_err, v_err)
     v_eigen, m_eigen = np.linalg.eig(m_cov)
-    m_c = np.einsum('ij,i->ij', np.eye(v_eigen.size), (1 / np.sqrt(v_eigen)))
-    m_a = np.einsum('ik,kj', m_c, m_eigen)
+    m_c = np.einsum("ij,i->ij", np.eye(v_eigen.size), (1 / np.sqrt(v_eigen)))
+    m_a = np.einsum("ik,kj", m_c, m_eigen)
     m_a_inv = np.linalg.inv(m_a)
 
     print()
-    print(f'{process}{model} shifted central values:')
+    print(f"{process}{model} shifted central values:")
     for i in range(5):
-        var_values = m_a_inv[:,i]
+        var_values = m_a_inv[:, i]
         # compute shift for ap's
         var_ap = var_values[:3]
         var_ap = np.append(var_ap, 0.0)
         # compute shift for a0'
         var_a0 = var_values[3:]
-        var_a00 = np.dot(np.array(add_params['a00']), var_values)
+        var_a00 = np.dot(np.array(add_params["a00"]), var_values)
         var_a0 = np.insert(var_a0, 0, var_a00)
         var_a0 = np.append(var_a0, 0.0)
 
         if verbose:
-            print(f'  // shifting {i+1}-th param in + direction....')
-            print('  {')
-        print_param_general(process+model, 'ap', list(np.array(ap) + var_ap))
-        print_param_general(process+model, 'a0', list(np.array(a0) + var_a0))
+            print(f"  // shifting {i+1}-th param in + direction....")
+            print("  {")
+        print_param_general(process + model, "ap", list(np.array(ap) + var_ap))
+        print_param_general(process + model, "a0", list(np.array(a0) + var_a0))
         if verbose:
-            print('  },')
+            print("  },")
 
         if verbose:
-            print(f'  // shifting {i+1}-th param in - direction....')
-            print('  {')
-        print_param_general(process+model, 'ap', list(np.array(ap) - var_ap))
-        print_param_general(process+model, 'a0', list(np.array(a0) - var_a0))
+            print(f"  // shifting {i+1}-th param in - direction....")
+            print("  {")
+        print_param_general(process + model, "ap", list(np.array(ap) - var_ap))
+        print_param_general(process + model, "a0", list(np.array(a0) - var_a0))
         if verbose:
-            print('  },')
+            print("  },")
 
 
 def gen_param_shifted_BtoDstarBGL(
-    process, model, m_corr, avec, bvec, cvec, dvec, aerr, berr, cerr, derr,
-    scale, verbose=True
+    process,
+    model,
+    m_corr,
+    avec,
+    bvec,
+    cvec,
+    dvec,
+    aerr,
+    berr,
+    cerr,
+    derr,
+    scale,
+    verbose=True,
 ):
     m_corr = np.matrix(m_corr)
     # scale error w/ Vcb * eta_ew as well!
     v_err = np.array(aerr + berr + cerr + derr) * scale
 
-    m_cov = np.einsum('ij,i,j->ij', m_corr, v_err, v_err)
+    m_cov = np.einsum("ij,i,j->ij", m_corr, v_err, v_err)
     v_eigen, m_eigen = np.linalg.eig(m_cov)
-    m_c = np.einsum('ij,i->ij', np.eye(v_eigen.size), (1 / np.sqrt(v_eigen)))
-    m_a = np.einsum('ik,kj', m_c, m_eigen)
+    m_c = np.einsum("ij,i->ij", np.eye(v_eigen.size), (1 / np.sqrt(v_eigen)))
+    m_a = np.einsum("ik,kj", m_c, m_eigen)
     m_a_inv = np.linalg.inv(m_a)
 
     print()
-    print(f'{process}{model} shifted central values:')
+    print(f"{process}{model} shifted central values:")
     for i in range(10):
-        var_values = m_a_inv[:,i]
+        var_values = m_a_inv[:, i]
         var_a = var_values[:3]
         var_b = var_values[3:6]
         var_c = var_values[6:8]
         var_d = var_values[8:10]
 
         if verbose:
-            print(f'  // shifting {i+1}-th param in + direction....')
-            print('  {')
-        for name, nom, delta in zip(['avec', 'bvec', 'cvec', 'dvec'],
-                                    [avec, bvec, cvec, dvec],
-                                    [var_a, var_b, var_c, var_d]):
-            print_param_general(
-                process+model, name, list(np.array(nom) + delta))
+            print(f"  // shifting {i+1}-th param in + direction....")
+            print("  {")
+        for name, nom, delta in zip(
+            ["avec", "bvec", "cvec", "dvec"],
+            [avec, bvec, cvec, dvec],
+            [var_a, var_b, var_c, var_d],
+        ):
+            print_param_general(process + model, name, list(np.array(nom) + delta))
         if verbose:
-            print('  },')
+            print("  },")
 
         if verbose:
-            print(f'  // shifting {i+1}-th param in - direction....')
-            print('  {')
-        for name, nom, delta in zip(['avec', 'bvec', 'cvec', 'dvec'],
-                                    [avec, bvec, cvec, dvec],
-                                    [var_a, var_b, var_c, var_d]):
-            print_param_general(
-                process+model, name, list(np.array(nom) - delta))
+            print(f"  // shifting {i+1}-th param in - direction....")
+            print("  {")
+        for name, nom, delta in zip(
+            ["avec", "bvec", "cvec", "dvec"],
+            [avec, bvec, cvec, dvec],
+            [var_a, var_b, var_c, var_d],
+        ):
+            print_param_general(process + model, name, list(np.array(nom) - delta))
         if verbose:
-            print('  },')
+            print("  },")
 
 
 def gen_param_shifted_BtoDstarstarBLR(
-        process, model, m_corr, v_nom, v_err, param_names, verbose=True):
+    process, model, m_corr, v_nom, v_err, param_names, verbose=True
+):
     m_corr = np.matrix(m_corr)
     v_err = np.array(v_err)
 
-    m_cov = np.einsum('ij,i,j->ij', m_corr, v_err, v_err)
+    m_cov = np.einsum("ij,i,j->ij", m_corr, v_err, v_err)
     v_eigen, m_eigen = np.linalg.eig(m_cov)
-    m_c = np.einsum('ij,i->ij', np.eye(v_eigen.size), (1 / np.sqrt(v_eigen)))
-    m_a = np.einsum('ik,kj', m_c, m_eigen)
+    m_c = np.einsum("ij,i->ij", np.eye(v_eigen.size), (1 / np.sqrt(v_eigen)))
+    m_a = np.einsum("ik,kj", m_c, m_eigen)
     m_a_inv = np.linalg.inv(m_a)
 
     print()
-    print(f'{process}{model} shifted central values:')
+    print(f"{process}{model} shifted central values:")
     for i in range(len(v_nom)):
-        var_values = m_a_inv[:,i]
+        var_values = m_a_inv[:, i]
 
         if verbose:
-            print(f'  // shifting {i+1}-th param in + direction....')
-            print('  {')
+            print(f"  // shifting {i+1}-th param in + direction....")
+            print("  {")
         for name, nom, delta in zip(param_names, v_nom, var_values):
             # multiplying deltas by 2, because don't fully trust paper constraints
-            print_param_general(process+model, name, nom + 2*delta)
+            print_param_general(process + model, name, nom + 2 * delta)
         if verbose:
-            print('  },')
+            print("  },")
 
         if verbose:
-            print(f'  // shifting {i+1}-th param in - direction....')
-            print('  {')
+            print(f"  // shifting {i+1}-th param in - direction....")
+            print("  {")
         for name, nom, delta in zip(param_names, v_nom, var_values):
-            print_param_general(process+model, name, nom - 2*delta)
+            print_param_general(process + model, name, nom - 2 * delta)
         if verbose:
-            print('  },')
+            print("  },")
 
 
 ########
 # Main #
 ########
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = parse_input()
 
     with open(args.input) as f:
         cfg = yaml.safe_load(f)
 
     for ff_model in [m for m in args.modes if m in cfg]:
-        print(f'Handling {ff_model}...')
+        print(f"Handling {ff_model}...")
         for param, val in cfg[ff_model].items():
-            if param.startswith('_'):
+            if param.startswith("_"):
                 continue  # temp variables, skip them
 
             if not isinstance(val, str):
-                print_param_general(ff_model, param, val)
+                print_param_general(ff_model, param, val, True)
             else:
                 result = eval_fake_sandbox(val, cfg[ff_model])
                 if result:
-                    print_param_general(ff_model, param, result)
+                    print_param_general(ff_model, param, result, True)
                     cfg[ff_model][param] = result
         print()
